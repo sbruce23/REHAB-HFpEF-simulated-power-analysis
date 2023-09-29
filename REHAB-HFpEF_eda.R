fdf448c9b99562c68ca748608e31ee87f7cbab69
Scott A. Bruce <scott.bruce@ymail.com>
2023-09-29 00:58:26 -0500

    Add files via upload

---
+++ REHAB-HFpEF_eda.R
@@ -0,0 +1,159 @@
+rm(list=ls())
+
+library(tidyverse)
+library(AER)
+library(fitdistrplus)
+
+###################
+# 1. read in data #
+###################
+setwd("/Users/scottbruce/Library/CloudStorage/OneDrive-SharedLibraries-HarvardUniversity/REHAB-HFpEF Ancillary - General/REHAB-HF Power Analysis")
+load("rprotblds.RData")
+
+#####################################################
+# 2. EDA for primary outcome (number of rehospitalizations and death events in 6 months)
+####################################################
+
+# custom theme for ggplot
+hw <- theme_gray()+ theme(
+  plot.title=element_text(hjust=0.5,size=18,face="bold"),
+  plot.subtitle=element_text(hjust=0.5,size=12),
+  plot.caption=element_text(hjust=-.5,size=10),
+  strip.background=element_rect(fill=rgb(.9,.95,1),
+                                colour=gray(.5), linewidth=.2),
+  panel.border=element_rect(fill=FALSE,colour=gray(.70)),
+  panel.grid.minor.y = element_blank(),
+  panel.grid.minor.x = element_blank(),
+  panel.spacing.x = unit(0.2,"cm"),
+  panel.spacing.y = unit(0.2,"cm"),
+  axis.text=element_text(colour="black",size=10),
+  axis.text.y=element_text(margin=ggplot2::margin(0,3,0,3)),
+  axis.text.x=element_text(margin=ggplot2::margin(-1,0,3,0)),
+  axis.title=element_text(size=16,face="bold"),
+  legend.text=element_text(size=14),
+  legend.title = element_blank(),
+);
+
+# check for overdispersion
+mean(rprotblds$nrehosp_death) #1.300412
+var(rprotblds$nrehosp_death) #2.268884
+ggplot(data=rprotblds,mapping=aes(x=nrehosp_death))+geom_bar()
+# variance is larger than mean, so potentially overdisperse
+
+# test for overdispersion with covariates only (no proteins)
+mod1=glm(data=rprotblds,formula=nrehosp_death ~ age + sex + race___4 + hf_cat
+         + intervention_1_control_0,family='poisson')
+dispersiontest(mod1)
+# overdisperse conditionally with disperson of 1.682113
+
+# reference for dispersion test:
+#Cameron, A.C. and Trivedi, P.K. (1990). Regression-based Tests for Overdispersion
+# in the Poisson Model. Journal of Econometrics, 46, 347–364.
+
+# first build independent regression models for each protein individually
+# and make volcano plot
+intxn_df = matrix(NA,nrow=92,ncol=2)
+colnames(intxn_df) = c("effect","p-value")
+for (i in 3:94){
+  prot=names(rprotblds)[i]
+  formtmp=paste("nrehosp_death ~ age + sex + race___4 + hf_cat + `",prot,
+             "`*intervention_1_control_0",sep="")
+  tmpmod=glm(data=rprotblds,formula=formtmp,family='quasipoisson')
+  intxn_df[i-2,]=coef(summary(tmpmod))[length(tmpmod$coefficients),c(1,4)]
+}
+
+intxn_df_all = data.frame(Protein=names(rprotblds)[3:94],intxn_df,
+                      Significance=ifelse(intxn_df[,2]<0.05,"Significant","Not Significant"))
+
+pdf("nrehospdeath_volcano_effectmod.pdf", onefile = TRUE)
+ggplot(data=intxn_df_all,mapping=aes(x=effect,y=-log10(p.value),color=Significance))+geom_point()+
+  geom_hline(yintercept = -log10(0.05), linetype="dotted")+
+  labs(x = "Effect modification", y = "-log10(p-value)") +
+  OlinkAnalyze::set_plot_theme() + ggtitle('NRD Effect Modification (Unadjusted P-values)')+
+  ggrepel::geom_label_repel(data = intxn_df_all[intxn_df_all$Significance=="Significant",],
+                            ggplot2::aes(label = Protein), box.padding = 1, show.legend = FALSE,max.overlaps=Inf) 
+dev.off()
+
+#PI3 only one that is significant (unadjusted for multiple testing), so visualize treatment effect modification
+pdf("nrehospdeath_PI3_effectmod.pdf", onefile = TRUE)
+ggplot(data=rprotblds,mapping=aes(x=`PI3`,y=nrehosp_death,color=intervention_1_control_0))+
+  geom_point()+geom_smooth(method='glm',se=TRUE,method.args=list(family='quasipoisson'))+
+  ylab("Number of Rehospitalizations and Death")+xlab("Baseline PI3")+
+  scale_color_discrete(labels=c('Control', 'Intervention'))+
+  theme(legend.title= element_blank())
+dev.off()
+
+#####################################################
+# 3. Investigate distribution of protein expression levels
+####################################################
+
+pdf("proteinexpressiondistributions.pdf", onefile = TRUE)
+# Use a multivariate normal distribution to simulate protein expression levels for 3,072 proteins.
+# We will first approximate the distribution of the mean protein expression levels, as well as the variances
+# and covariances of the protein expression levels using pilot data on 92 proteins.  
+
+# Distribution of mean protein expression levels across proteins
+summary(fitdist(as.numeric(apply(rprotblds[,3:94],2,function(x) mean(x,na.rm=TRUE))),distr='gamma',method='mme'))
+plot(fitdist(as.numeric(apply(rprotblds[,3:94],2,function(x) mean(x,na.rm=TRUE))),distr='gamma',method='mme'))#draw means for protein expressions from normal(mean=5.2,sd=2.1)
+#draw means from gamma with shape 6.3 and rate 1.2
+
+#Distribution of variances of protein expression levels across proteins
+summary(fitdist(as.numeric(apply(rprotblds[,3:94],2,function(x) var(x,na.rm=TRUE))),distr='lnorm',method='mme'))
+plot(fitdist(as.numeric(apply(rprotblds[,3:94],2,function(x) var(x,na.rm=TRUE))),distr='lnorm',method='mme'))#draw means for protein expressions from normal(mean=5.2,sd=2.1)
+#draw variances from log normal with meanlog -0.87 and sdlog=0.76
+
+#Distribution of covariances of protein expression levels across proteins
+covs=cov(rprotblds[,3:94])[lower.tri(cov(rprotblds[,3:94]))]
+covs=covs[!is.na(covs)]
+summary(fitdist(covs,distr='norm',method='mme'))
+plot(fitdist(covs,distr='norm',method='mme'))#draw means for protein expressions from normal(mean=5.2,sd=2.1)
+#draw covariances from normal mean=0.095, sd=0.10
+
+dev.off()
+
+#####################################################
+# 4. Explore treatment and covariate effects in pilot data
+####################################################
+
+#Estimate treatment and covariate effects across protein-specific models
+trtmt_df = matrix(NA,nrow=92,ncol=2)
+age_df = matrix(NA,nrow=92,ncol=2)
+sex_df = matrix(NA,nrow=92,ncol=2)
+race_df = matrix(NA,nrow=92,ncol=2)
+hfcat_df = matrix(NA,nrow=92,ncol=2)
+
+for (i in 3:94){
+  prot=names(rprotblds)[i]
+  formtmp=paste("nrehosp_death ~ age + sex + race___4 + hf_cat + `",prot,
+                "`*intervention_1_control_0",sep="")
+  tmpmod=glm(data=rprotblds,formula=formtmp,family='quasipoisson')
+  trtmt_df[i-2,]=coef(summary(tmpmod))[length(tmpmod$coefficients)-1,c(1,4)]
+  age_df[i-2,]=coef(summary(tmpmod))[2,c(1,4)]
+  sex_df[i-2,]=coef(summary(tmpmod))[3,c(1,4)]
+  race_df[i-2,]=coef(summary(tmpmod))[4,c(1,4)]
+  hfcat_df[i-2,]=coef(summary(tmpmod))[5,c(1,4)]
+}
+
+pdf("covariateeffects.pdf", onefile = TRUE)
+
+ggplot(data=as.data.frame(trtmt_df),mapping=aes(x=V1))+geom_histogram()+
+  labs(x="Treatment Effect")+geom_vline(xintercept=median(trtmt_df[,1]),color='red',linetype='dashed')
+summary(trtmt_df[,1]) #median is 0.46
+
+ggplot(data=as.data.frame(age_df),mapping=aes(x=V1))+geom_histogram()+
+  labs(x="Age Effect")+geom_vline(xintercept=median(age_df[,1]),color='red',linetype='dashed')
+summary(age_df[,1]) #median is 0.0002
+
+ggplot(data=as.data.frame(sex_df),mapping=aes(x=V1))+geom_histogram()+
+  labs(x="Sex Effect")+geom_vline(xintercept=median(sex_df[,1]),color='red',linetype='dashed')
+summary(sex_df[,1]) #median is -0.06 (Sex=1 is female)
+
+ggplot(data=as.data.frame(race_df),mapping=aes(x=V1))+geom_histogram()+
+  labs(x="Race Effect")+geom_vline(xintercept=median(race_df[,1]),color='red',linetype='dashed')
+summary(race_df[,1]) #median is -0.41 (Race=1 is white)
+
+ggplot(data=as.data.frame(hfcat_df),mapping=aes(x=V1))+geom_histogram()+
+  labs(x="Heart Failure Category Effect")+geom_vline(xintercept=median(hfcat_df[,1]),color='red',linetype='dashed')
+summary(hfcat_df[,1]) #median is 0.16 (hfcat==1 is HFpEF) 
+
+dev.off()
---
+++ REHAB-HFpEF_simulatedpoweranalysis.R
@@ -0,0 +1,156 @@
+rm(list=ls())
+
+library(MASS)
+library(glmnet)
+library(tidyverse)
+
+###################
+# 1. read in data #
+###################
+setwd("/Users/scottbruce/Library/CloudStorage/OneDrive-SharedLibraries-HarvardUniversity/REHAB-HFpEF Ancillary - General/REHAB-HF Power Analysis")
+load("rprotblds.RData")
+
+#####################################################
+# 2. simulation based power analysis
+####################################################
+
+# input parameters
+set.seed(435) #seed for reproducibility
+nsamp_true=704 #sample size
+nrep=100 #number of replicates
+
+# function to generate random draws from quasi-poisson
+rqpois <- function(n, mu, theta) {
+  rnbinom(n = n, mu = mu, size = mu/(theta-1))
+}
+
+# initialize data containers
+simdata=list()
+lassomod=list()
+lassofit=list()
+pow=list()
+fdp=list()
+
+# generate multivariate normal to be used to simulate 3,072 protein expression levels 
+mus=rgamma(3072,shape=6.3,rate=1.2)
+vars=rlnorm(3072,meanlog=-0.87,sdlog=0.76)
+covs=rnorm((3072^2-3072)/2,mean=0.095,sd=0.10)
+
+# find nearest positive definite covariance matrix
+sigma=diag(vars)
+sigma[lower.tri(sigma)]=covs
+sigma[upper.tri(sigma)]=covs
+sigma = (sigma + t(sigma))/2
+sigma.evd=eigen(sigma)
+sigma.psd = sigma.evd$vectors %*% diag(pmax(sigma.evd$values,0)) %*% t(sigma.evd$vectors)
+
+# simulate data and fit LASSO for each replicate
+for (r in 1:nrep){
+
+  # bootstrap draw nsamp patients from pilot data with covariates
+  nsamp=nsamp_true*2 #generating more samples since some draws are discarded due to having too high event rates
+  df=rprotblds[sample(1:nrow(rprotblds),size=nsamp,replace=TRUE),c(1,2,96:99,115)]
+  
+  # simulate protein expression levels from multivariate normal
+  df.prot=mvrnorm(n=nsamp,mu=mus,Sigma=sigma.psd)
+  names(df.prot) = paste("protein",1:3072,sep="_")
+  df.all=cbind(df,df.prot)
+  colnames(df.all)[8:3079]= paste("protein",1:3072,sep="_")
+  
+  # simulate pseudo-responses for 6-month number of rehospitalizations or death
+  respmus=numeric(length=nsamp)
+  pseudoresp=numeric(length=nsamp)
+  for (i in 1:nsamp){
+    logmu=0.002*df.all$age[i]-0.006*(df.all$sex[i]==1)-0.41*(df.all$race___4[i]==1)+
+      0.16*(df.all$hf_cat[i]==1)+ # covariates
+      # treatment effect
+      (df.all$intervention_1_control_0[i]==1)*(1-0.3*df.all$protein_1[i])+
+      (df.all$intervention_1_control_0[i]==1)*(1-0.3*df.all$protein_2[i])+
+      (df.all$intervention_1_control_0[i]==1)*(1-0.3*df.all$protein_3[i])+
+      # protein effect under control only
+      (df.all$intervention_1_control_0[i]==0)*(-3+0.62*df.all$protein_1[i])+
+      (df.all$intervention_1_control_0[i]==0)*(-3+0.62*df.all$protein_2[i])+
+      (df.all$intervention_1_control_0[i]==0)*(-3+0.62*df.all$protein_3[i])
+  
+    respmus[i]=exp(logmu)
+    
+    # generate pseudoresponses from quasi-poisson with 1.7 dispersion (matching pilot data)
+    pseudoresp[i]=rqpois(1,respmus[i],1.7)
+  }
+  
+  # drop observations that have an event rate above 10 (too high for 6 months)
+  if(length(which(respmus>10))>0){
+    df.final=df.all[-which(respmus>10),] 
+    pseudoresp=pseudoresp[-which(respmus>10)]
+    respmus=respmus[-which(respmus>10)]
+  } else{
+    df.final=df.all
+  }
+  
+  # drop uneeded extra draws to get down to original sample size
+  idx=sample(1:nrow(df.final),nsamp_true)
+  df.final=df.final[idx,]
+  respmus=respmus[idx]
+  pseudoresp=pseudoresp[idx]
+  
+  # combine everything into a single dataframe
+  pseudo_data <- cbind(pseudoresp,df.final[,c("intervention_1_control_0","age","sex","race___4","hf_cat")],
+                         df.final[,8:3079])
+  simdata[[r]] = pseudo_data
+  
+  # use LASSO to estimate model parameters
+  f <- as.formula(pseudoresp ~ .+intervention_1_control_0*.)
+  pseudo_y <- pseudo_data$pseudoresp
+  pseudo_x <- model.matrix(f, pseudo_data)[, -1]
+
+  fit <- cv.glmnet(pseudo_x, pseudo_y,family=quasipoisson())
+  plot(fit)
+  lassomod[[r]]=fit
+  lassofit[[r]]=coef(fit,lamba=fit$lambda.min)[which(coef(fit,lambda=fit$lambda.min)!=0),]
+  
+  # get power and fdp for the run 
+  # (correct identification of emm if both main effect and interaction terms are significant)
+  pow[[r]] = ((sum(coef(fit,lamba=fit$lambda.min)[c(7,3083)]!=0)==2) +
+    (sum(coef(fit,lamba=fit$lambda.min)[c(8,3084)]!=0)==2)+
+      (sum(coef(fit,lamba=fit$lambda.min)[c(9,3085)]!=0)==2))/3
+  
+  tmpfdp=0
+  for (i in 4:3072){
+    tmpfdp = tmpfdp+(sum(coef(fit,lamba=fit$lambda.min)[c(6+i,3082+i)]!=0)==2)/3069
+  }
+  fdp[[r]]=tmpfdp
+  
+  print(paste("Replicate:",r,", Power:",pow[[r]],", FDP:",fdp[[r]]))
+  
+  save.image("PowerAnalysis_092823.RData")
+  
+  # plot single run pseudoresponse distribution and protein EMM behavior
+  if (r==1){
+    
+    pdf("pseudoresponse_distribution.pdf", onefile = TRUE)
+    
+    # visualize distribution of pseudoresponse by treatment
+    ggplot(data=data.frame(pseudoresp=pseudoresp,trt=df.final$intervention_1_control_0),
+           mapping=aes(x=pseudoresp,fill=trt))+geom_histogram(aes(y = after_stat(count / sum(count))), 
+                                                              position='identity',alpha=0.4)+
+      labs(x="Pseudo Response (6-month # hospitalizations or death)",
+           y="Proportion",title="Distribution of Pseudo Response by Treatment")
+    
+    # visualize treatment effect modification for EMM proteins (1,2,3) and non-EMM proteins (4,5)
+    p=list()
+    for (i in 1:5){
+      prot=names(df.final)[1:15+7][i]
+      p[[i]]=ggplot(data=df.final,mapping=aes(x=!!ensym(prot),y=pseudoresp,
+                                              color=intervention_1_control_0))+geom_point()+
+        geom_smooth(method='glm',method.args=list(family='quasipoisson'))+
+        labs(title="Treatment Effect Modification")
+      print(p[[i]])
+    }
+    dev.off()
+  }
+
+}
+
+# output power and FDR
+mean(unlist(pow))
+mean(unlist(fdp))
